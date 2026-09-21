0225e6f6e6ec7f80e48e32f5d27768cf7c20ffb5
daisycho623 <daisycho623@gmail.com>
2026-09-21 12:40:13 +0900

    Apps Script 새 배포

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script를 웹 앱으로 배포한 후 생성된 /exec URL을 입력하세요.
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw66vCm2h5kxGaR-JcIUn650FphIblm8xys6gZ8YYW68Oqv2nyMrvtCata91uO8P-4lAA/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw3ZxIPlMjezIgXX-V_OK2Mzs0_c39rd3_Szf4J3ydnbee9Lw502y-oRO5mkqaKqOaG3g/exec';
