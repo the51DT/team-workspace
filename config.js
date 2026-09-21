0d0f1067eb108973ecddd7d3819c2aa7d98c7be4
daisycho623 <daisycho623@gmail.com>
2026-09-21 14:04:11 +0900

    로그인 계정 관리

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script를 웹 앱으로 배포한 후 생성된 /exec URL을 입력하세요.
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw3ZxIPlMjezIgXX-V_OK2Mzs0_c39rd3_Szf4J3ydnbee9Lw502y-oRO5mkqaKqOaG3g/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1Ja2TYcrSTd2njmlOHTRjdwEqZpfD88xhYiC4yo24dV5tFyP1zVPSFWDxyY3GZzu6Jw/exec';
