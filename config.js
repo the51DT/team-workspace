a4b0752d1a98780736dce9ab4cde705ebc022ebd
daisycho623 <daisycho623@gmail.com>
2026-09-24 07:56:06 +0900

    수정 이력 변경 전 / 변경 후 방식으로 변경

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script를 웹 앱으로 배포한 후 생성된 /exec URL을 입력하세요.
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxzbjZ5uq5vKMjg8pRPA6DNT6WB8uEGHZUPQCycznxDJZvFtmVikj_zJY9pmEeX53sc/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzw8b2VfF9k8eGOwEtg78cejg0VQG5VBsOG3wJhqwkLe8OijyCx7frfQtHuf55msGDOTA/exec';
